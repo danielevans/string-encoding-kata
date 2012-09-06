d297b977511b87ddc0bb2d68979b0f2c0bb7f856
Daniel Evans <evans.daniel.n@gmail.com>
2012-09-06 01:56:22 -0600

    Iterate

--- decoder.rb
+++ decoder.rb
@@ -1,5 +1,22 @@
 class Decoder
   def self.decode(encoded_str)
-    ("a".ord + encoded_str.to_i - 1).chr
+    return [] if encoded_str.empty?
+
+    decodings = []
+    26.times do |i|
+      char = (i + "a".ord).chr
+      encoded_char = (i + 1).to_s
+
+      if encoded_str.start_with?(encoded_char) && !encoded_str.start_with?(encoded_char + "0")
+        if encoded_str == encoded_char
+          decodings.push char
+        else
+          decode(encoded_str.gsub(/^#{encoded_char}/, '')).each do |possability|
+            decodings.push "#{char}#{possability}"
+          end
+        end
+      end
+    end
+    decodings
   end
 end
--- spec/decoder_spec.rb
+++ spec/decoder_spec.rb
@@ -3,24 +3,40 @@ require 'spec_helper'
 
 describe Decoder do
   describe ".decode" do
-    it "returns a string" do
-      Decoder.decode("1").should be_a(String)
+    it "returns an array" do
+      Decoder.decode("1").should be_a(Array)
     end
 
-    it "1 decodes to a" do
-      Decoder.decode("1").should == "a"
+    it "decodes empty string to empty array" do
+      Decoder.decode("").should == []
     end
 
-    it "2 decodes to b" do
-      Decoder.decode("2").should == "b"
+    it "decodes 1 to a" do
+      Decoder.decode("1").should == ["a"]
     end
 
-    it "3 decodes to c" do
-      Decoder.decode("3").should == "c"
+    it "decodes 2 to b" do
+      Decoder.decode("2").should == ["b"]
     end
 
-    it "26 decodes to z" do
-      Decoder.decode("26").should == "z"
+    it "decodes 3 to c" do
+      Decoder.decode("3").should == ["c"]
+    end
+
+    it "decodes ambiguous strings" do
+      Decoder.decode("11").should == ["aa","k"]
+    end
+
+    it "decodes very ambiguous strings" do
+      Decoder.decode("2222").should == ['bbbb','bbv','bvb','vbb','vv']
+    end
+
+    it "properly ignores leading zero problems" do
+      Decoder.decode("101").should == ["ja"]
+    end
+
+    it "returns empty array for impossible encodings" do
+      Decoder.decode("0000").should == []
     end
   end
 end
